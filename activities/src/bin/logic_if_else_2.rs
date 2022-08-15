78073c4f5da6e6415514c4efea772c7d5fb4a30b
Kobe Ruado <insidiae423@gmail.com>
2022-08-15 18:06:34 +0800

    Finish Making Decisions with Rust section

---
+++ activities/src/bin/logic_if_else_2.rs
@@ -0,0 +1,20 @@
+// Topic: Flow control using if..else if..else
+//
+// Program requirements:
+// * Display ">5", "<5", or "=5" based on the value of a variable
+//   is > 5, < 5, or == 5, respectively
+
+fn main() {
+	// * Use a variable set to any integer value
+	let n = 5;
+
+	// * Use an if..else if..else block to determine which message to display
+	if n > 5 {
+		// * Use the println macro to display messages to the terminal
+		println!(">5");
+	} else if n < 5 {
+		println!("<5");
+	} else {
+		println!("=5");
+	}
+}

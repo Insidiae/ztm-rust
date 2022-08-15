864b68a9a15c233667b45b1f8247b64190650115
Kobe Ruado <insidiae423@gmail.com>
2022-08-15 18:38:16 +0800

    Finish Repetition section

--- activities/readme.md
+++ activities/readme.md
@@ -4,3 +4,11 @@
 
 1. [Functions](./src/bin/functions.rs)
 2. [Basic Math](./src/bin/basic_math.rs)
+3. Logic with If & Else
+   - [Part 1](./src/bin/logic_if_else_1.rs)
+   - [Part 2](./src/bin/logic_if_else_2.rs)
+4. Basic Match
+   - [Part 1](./src/bin/basic_match_1.rs)
+   - [Part 2](./src/bin/basic_match_2.rs)
+5. [Loops](./src/bin/loops.rs)
+6. [While Loops](./src/bin/while_loops.rs)
---
+++ activities/src/bin/loops.rs
@@ -0,0 +1,23 @@
+// Topic: Looping using the loop statement
+//
+// Program requirements:
+// * Display "1" through "4" in the terminal
+
+fn main() {
+	// * Use a mutable integer variable
+	let mut n = 1;
+
+	// * Use a loop statement
+	loop {
+		// * Print the variable within the loop statement
+		println!("{:?}", n);
+
+		if n == 4 {
+			// * Use break to exit the loop
+			break;
+		}
+
+		// n = n + 1;
+		n += 1;
+	}
+}

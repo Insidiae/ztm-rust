60232c38dbcf75fd91997dbddab3f0ef99b0cd74
Kobe Ruado <insidiae423@gmail.com>
2022-08-28 02:41:16 +0800

    ClipStash: Finish Service Layer Implementation

--- clipstash/src/lib/data/model.rs
+++ clipstash/src/lib/data/model.rs
@@ -39,6 +39,14 @@ pub struct GetClip {
 	pub(in crate::data) shortcode: String,
 }
 
+impl From<crate::service::ask::GetClip> for GetClip {
+	fn from(req: crate::service::ask::GetClip) -> Self {
+		Self {
+			shortcode: req.shortcode.into_inner(),
+		}
+	}
+}
+
 impl From<ShortCode> for GetClip {
 	fn from(shortcode: ShortCode) -> Self {
 		Self {
@@ -59,8 +67,22 @@ pub struct NewClip {
 	pub(in crate::data) content: String,
 	pub(in crate::data) title: Option<String>,
 	pub(in crate::data) posted: i64,
-	pub(in crate::data) expires: Option<NaiveDateTime>,
+	pub(in crate::data) expires: Option<i64>,
 	pub(in crate::data) password: Option<String>,
+}
+
+impl From<crate::service::ask::NewClip> for NewClip {
+	fn from(req: crate::service::ask::NewClip) -> Self {
+		Self {
+			clip_id: DbId::new().into(),
+			shortcode: ShortCode::default().into(),
+			content: req.content.into_inner(),
+			title: req.title.into_inner(),
+			posted: Utc::now().timestamp(),
+			expires: req.expires.into_inner().map(|time| time.timestamp()),
+			password: req.password.into_inner(),
+		}
+	}
 }
 
 pub struct UpdateClip {
@@ -69,4 +91,16 @@ pub struct UpdateClip {
 	pub(in crate::data) title: Option<String>,
 	pub(in crate::data) expires: Option<i64>,
 	pub(in crate::data) password: Option<String>,
+}
+
+impl From<crate::service::ask::UpdateClip> for UpdateClip {
+	fn from(req: crate::service::ask::UpdateClip) -> Self {
+		Self {
+			shortcode: req.shortcode.into_inner(),
+			content: req.content.into_inner(),
+			title: req.title.into_inner(),
+			expires: req.expires.into_inner().map(|time| time.timestamp()),
+			password: req.password.into_inner(),
+		}
+	}
 }
